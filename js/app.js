28ed0959d86cf3f1f8e6ac595cb1a72d25a77f6e
Alexander Malevany <malevany9@gmail.com>
2026-04-07 21:16:22 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+const gallerySlider = () => {
+    const sectionClass = '.production-gallery';
+    const sections = document.querySelectorAll(sectionClass);
+
+    sections.forEach(section => {
+        const sliderEl = section.querySelector('.swiper');
+        if (!sliderEl) return;
+
+        const slides = section.querySelectorAll('.swiper-slide');
+        const nextBtn = section.querySelector('.swiper-button-next');
+        const prevBtn = section.querySelector('.swiper-button-prev');
+
+        if (slides.length > 0) {
+            new Swiper(sliderEl, {
+                loop: slides.length > 4,
+                slidesPerView: 4,
+                spaceBetween: 20,
+                speed: 600,
+                watchOverflow: true,
+                grabCursor: true,
+
+                navigation: {
+                    nextEl: nextBtn,
+                    prevEl: prevBtn
+                },
+
+                breakpoints: {
+                    0: { slidesPerView: 1.2 },
+                    600: { slidesPerView: 2 },
+                    1000: { slidesPerView: 3 },
+                    1200: { slidesPerView: 4 }
+                }
+            });
+        }
+    });
+}
+gallerySlider();
