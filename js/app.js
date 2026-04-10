fe5a4d0cc5f2a449407aa816a319594dce80ea32
Alexander Malevany <malevany9@gmail.com>
2026-04-10 16:40:07 +0300

    add last page

--- js/app.js
+++ js/app.js
@@ -35,3 +35,40 @@ const gallerySlider = () => {
     });
 }
 gallerySlider();
+
+const manufacturersSlider = () => {
+    const sectionClass = '.manufacturers';
+    const sections = document.querySelectorAll(sectionClass);
+
+    sections.forEach(section => {
+        const sliderEl = section.querySelector('.swiper');
+        if (!sliderEl) return;
+
+        const slides = section.querySelectorAll('.swiper-slide');
+        const nextBtn = section.querySelector('.swiper-button-next');
+        const prevBtn = section.querySelector('.swiper-button-prev');
+
+        if (slides.length > 0) {
+            new Swiper(sliderEl, {
+                loop: false,
+                slidesPerView: 6,
+                spaceBetween: 20,
+                speed: 600,
+                watchOverflow: true,
+
+                navigation: {
+                    nextEl: nextBtn,
+                    prevEl: prevBtn
+                },
+
+                breakpoints: {
+                    0: { slidesPerView: 2.2 },
+                    600: { slidesPerView: 3 },
+                    1000: { slidesPerView: 4 },
+                    1200: { slidesPerView: 6 }
+                }
+            });
+        }
+    });
+}
+manufacturersSlider();
